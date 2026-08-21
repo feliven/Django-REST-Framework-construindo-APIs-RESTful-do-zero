5706076fd36b50c14315b21779f6b101638e72b3
FVS-MBA <feliven@gmail.com>
2026-08-21 20:51:42 -0300

    cria models e realiza primeira migração de banco de dados

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Estudante(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50, blank=False)
+    CPF = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+    numero_celular = models.CharField(max_length=14)
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class Curso(models.Model):
+    OPCOES_NIVEL = [
+        ("B", "Básico"),
+        ("I", "Intermediário"),
+        ("A", "Avançado"),
+    ]
+
+    codigo = models.CharField(max_length=10)
+    descricao = models.TextField(
+        blank=False,
+    )
+    nivel = models.CharField(
+        max_length=1, blank=False, null=False, choices=OPCOES_NIVEL, default="B"
+    )
+
+    def __str__(self) -> str:
+        return self.codigo
