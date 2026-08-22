e0a3b4b6d6ec5dc022938c29e0e9c29bf1f1de8b
FVS-MBA <feliven@gmail.com>
2026-08-21 21:13:13 -0300

    configura página de admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from escola.models import Estudante, Curso
+
 
 # Register your models here.
+class ListandoEstudantes(admin.ModelAdmin):
+    list_display = ("id", "nome", "email")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_editable = ("email",)
+    list_per_page = 10
+
+
+class ListandoCursos(admin.ModelAdmin):
+    list_display = ("codigo", "descricao", "nivel")
+    list_display_links = ("codigo",)
+    search_fields = ("codigo", "descricao")
+    list_filter = ("nivel",)
+    list_editable = ("nivel",)
+    list_per_page = 10
+
+
+admin.site.register(Estudante, ListandoEstudantes)
+admin.site.register(Curso, ListandoCursos)
