2c89e9afd3c3b36e4de10c8f5f8adb52129d1443
FVS-MBA <feliven@gmail.com>
2026-08-22 14:30:09 -0300

    passa a exigir autenticação nas rotas

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics
-
-# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from escola.models import Estudante, Curso, Matricula
 from escola.serializers import (
     EstudanteSerializer,
@@ -14,19 +14,22 @@ from escola.serializers import (
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
-    # permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
-    # permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
-    # permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class MatriculasPorEstudante(generics.ListAPIView):
